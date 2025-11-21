882dae4802833aa7498f6a38cfb7ca9922e720fb
ObsidianSeal <bmgosselin@gmail.com>
2025-11-20 19:26:53 -0500

    pinniped.page activity

---
+++ websites/P/pinniped.page/presence.ts
@@ -0,0 +1,22 @@
+import { Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets { // Other default assets can be found at index.d.ts
+  Logo = '',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    smallImageKey: Assets.Play,
+    details: "pinniped.page",
+    
+  }
+
+  presence.setActivity(presenceData)
+})
